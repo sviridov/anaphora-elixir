d2e341466d4d541e9b33cf7fde8ffc2d4d0f879e
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-08-23 20:06:12 +0600

    Added: aif macro

--- lib/anaphorex.ex
+++ lib/anaphorex.ex
@@ -6,14 +6,14 @@ defmodule Anaphora do
   """
 
   @doc """
-  Returns `it` variable defined in user context
+  Returns the `it` variable defined in user context
   """
   defp it do
     Macro.var(:it, nil)
   end
 
   @doc """
-  Binds the `expression` to `it` (via `case`) in the scope of the `body`.
+  Binds the `expression` to `it` (via `case`) in the scope of the `body`
 
   ## Examples
 
@@ -25,11 +25,39 @@ defmodule Anaphora do
      ...>   tl(it)
      ...> end
      [3]
+
   """
   defmacro alet(expression, do: body) do
     quote do
       case unquote(expression) do
         unquote(it) -> unquote(body)
+      end
+    end
+  end
+
+  @doc """
+  Like `if`, except binds the result of the `condition` to `it` (via `alet`) for
+  the scope of the then and else `clauses`
+
+  ## Examples
+
+     iex> Anaphora.aif :aif_test, do: it
+     :aif_test
+
+     iex> Anaphora.aif 2 * 2 + 2 do
+     ...>   it / 2
+     ...> else
+     ...>   :never
+     ...> end
+     3.0
+
+     iex> Anaphora.aif 1 == 2, do: :never, else: it
+     false
+  """
+  defmacro aif(condition, clauses) do
+    quote do
+      Anaphora.alet unquote(condition) do
+        if(unquote(it), unquote(clauses))
       end
     end
   end
